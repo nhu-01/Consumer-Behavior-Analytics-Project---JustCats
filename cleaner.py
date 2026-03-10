c71f74a08020888ae1a51b2f89e7fb70e9caf629
LeetUnova <leetunova@gmail.com>
2026-03-10 11:13:15 -0700

    added loading a file from a commandline argument to cleaner.py as well as checking the number of commandline arguments presented

--- cleaner.py
+++ cleaner.py
@@ -1,14 +1,20 @@
 '''
-Student Name:
+Student Names: Zach, Nhu
 Date:
-
 '''
 
 import os
 import pandas as pd
 import sys
 
-#Load the file
+# Verify commandline arguments
+
+if len(sys.argv) < 2:
+    print(f"This file is used like this: 'python3 {__file__} [data.csv]'")
+
+# Load the file
+
+df = pd.read_csv(sys.argv[1])
 
 # Remove NaN values
 
