17605b9bb9959b3ddf88f86ec0b1f7ed1fe8adae
LeetUnova <leetunova@gmail.com>
2026-03-12 10:45:52 -0700

    Attempted fix for the type error in read_csv

--- cleaner.py
+++ cleaner.py
@@ -15,7 +15,45 @@ if len(sys.argv) < 2:
 
 # Load the file
 
-df = pd.read_csv(sys.argv[1])
+dtype: dict = {
+    "System Trans. ID": int,
+    "HHID": int,
+    "GeoID": int,
+    "Trans Date": str,
+    "Distinct Pro": int,
+    "Basket Units": int,
+    "Basket Dollar": float,
+    "Day-of-Week": str,
+    "Family Size": int,
+    "Race": str,
+    "Income": int,
+    "Home Ownership": str,
+    "Male Education": int,
+    "Male Occupation": str,
+    "Female Age": str,
+    "Female Work": str,
+    "Chldrn 0-5": str,
+    "Chldrn 6-11": str,
+    "Chldrn 12-17": str,
+    "Markets": str,
+    "Chains": str,
+    "Outlets": str,
+    "UPC": str,
+    "UPC Description": str,
+    "Category": int,
+    "Vendor": str,
+    "Brand": str,
+    "SKU Dollars": str,
+    "SKU Units": str,
+    "Size": float,
+    "SizeUnit": int,
+    "Unnamed: 31": int,
+    "Unnamed: 32": str,
+    "Unnamed: 33": str,
+    "Unnamed: 34": str
+}
+
+df = pd.read_csv(sys.argv[1], dtype=dtype)
 
 # Remove NaN values
 
